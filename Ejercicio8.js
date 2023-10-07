051c1266e77c9aac18fa6452da4dc1c1bb867baa
garvalius <jpferrer77@gmail.com>
2023-10-07 12:16:24 +0200

    Ejercicio 8

---
+++ Ejercicio8.js
@@ -0,0 +1,19 @@
+// @ts-nocheck
+let button8 = document.getElementById("Ejercicio_8_button");
+button8.addEventListener("click", makeExercise);
+
+
+
+function makeExercise() {
+
+    let names = ["Rafael", "Anselmo", "Ismael", "Eliseo", "Wenceslao", "Armando", "Jesús", "Manuel", "Pedro", "Gaspar"];
+
+    console.log(names.sort());
+
+}
+
+
+
+
+
+
